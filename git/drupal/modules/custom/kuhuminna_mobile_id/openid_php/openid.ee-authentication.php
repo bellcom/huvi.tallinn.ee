79ad3dc40dfcaf0046284e32307a44ab7a427df8
Juulia Devadze <jd@est1>
2016-05-30 21:14:28 +0200

    Openid patch added

--- git/drupal/modules/custom/kuhuminna_mobile_id/openid_php/openid.ee-authentication.php
+++ git/drupal/modules/custom/kuhuminna_mobile_id/openid_php/openid.ee-authentication.php
@@ -25,7 +25,7 @@ function escape($thing) { return htmlentities($thing); }
 function getConsumer() { $store = getStore(); $consumer = new Auth_OpenID_Consumer($store); return $consumer; }
 
 function &getStore() {
-    $store_path = "/tmp/_php_consumer_test";
+    $store_path = "../tmp/_php_consumer_test";
     if (!file_exists($store_path) && !mkdir($store_path)) {
         print "Could not create the FileStore directory '$store_path'. Please check the effective permissions.";
         exit(0);
