25eee74af8da029390d2fd899681ff88a729da65
Vovaks <rjabkovvladimir@gmail.com>
2016-05-31 14:05:40 +0200

    Change name Terve-Kogu

--- git/drupal/themes/huvi/templates/views/views-view-field--field-schedule-date.tpl.php
+++ git/drupal/themes/huvi/templates/views/views-view-field--field-schedule-date.tpl.php
@@ -2,7 +2,7 @@
 
 if (date("H:i", $row->field_data_field_schedule_date_field_schedule_date_value) == "23:55"){
   print '<span class="date-display-single">';
-  print t('Terve päev');
+  print t('Kogu päev');
   print '<span>';
 } 
 else
