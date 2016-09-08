2b76468c4a4d49acb1b2fc02a88ff3f62146a08d
Vovaks <rjabkovvladimir@gmail.com>
2016-09-08 08:22:36 +0300

    hide/show logi sisse

---
+++ git/drupal/modules/custom/kuhuminna_utils/kuhuminna_login.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(document).ready(function() {
+    $("#modalContent").on("click", "#logi_sisse", function(){
+      $("#div_logi_sisse").toggle('slow');
+    });
+  });
+})(jQuery);
