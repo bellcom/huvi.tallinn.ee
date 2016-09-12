aa509e4e3cbe8688f5c780f9d4d9fdf2fa80e36c
juuliabellcom <juulia@bellcom.dk>
2016-09-12 14:08:47 +0300

    Bug fixed

--- git/drupal/modules/custom/kuhuminna_utils/kuhuminna_login.js
+++ git/drupal/modules/custom/kuhuminna_utils/kuhuminna_login.js
@@ -1,5 +1,5 @@
 (function ($) {
-  $(document).ready(function() {
+  $(document).ajaxComplete(function() {
     $("#modalContent").on("click", "#logi_sisse", function(){
       //forbid second click
       if($("#div_logi_sisse").is(':visible')) {
