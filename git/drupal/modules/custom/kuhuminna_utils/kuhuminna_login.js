6bae0c91230bd5b44e54f41bffafc1a5478ae98b
Stanislav Kutasevits <stan@bellcom.dk>
2016-09-12 10:18:14 +0300

    refactoring

--- git/drupal/modules/custom/kuhuminna_utils/kuhuminna_login.js
+++ git/drupal/modules/custom/kuhuminna_utils/kuhuminna_login.js
@@ -2,12 +2,12 @@
   $(document).ready(function() {
     $("#modalContent").on("click", "#logi_sisse", function(){
       //forbid second click
-     if($("#div_logi_sisse").is(':visible'))
-        return False;
+      if($("#div_logi_sisse").is(':visible')) {
+       return false;
+      }
 
       $('#modalContent').height($('#modalContent').height() + $("#div_logi_sisse").height());
       $("#div_logi_sisse").toggle('slow');
-
     });
   });
 })(jQuery);
