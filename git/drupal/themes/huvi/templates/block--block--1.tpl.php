9f216358a16954d6066b7c161d92f1eb5a5d866c
Vovaks <rjabkovvladimir@gmail.com>
2017-01-17 16:10:11 +0200

    Chagne the name of adding an event

--- git/drupal/themes/huvi/templates/block--block--1.tpl.php
+++ git/drupal/themes/huvi/templates/block--block--1.tpl.php
@@ -1,8 +1,14 @@
 <?php
 $base_path = $GLOBALS['base_path'];
 $button_link = $base_path . 'user/login';
+$req_path = request_path();
 if(!in_array('anonymous user', $user->roles)) {
 	$button_link = $base_path . 'lisa';
+}
+if($req_path == "huvitegevused"){//check path, add change button name
+    $btn_lisa_text = 'Lisa oma huvitegevus';
+}else{
+    $btn_lisa_text = 'Lisa oma üritus';
 }
 ?>
 
@@ -10,7 +16,7 @@ if(!in_array('anonymous user', $user->roles)) {
   <h2 class="block__title"><?php print $block->title; ?></h2>
   <div class="block__content">
     <p><?php print t('Entering is easy and takes little time!'); ?>
-      <a class="button" href="<?php print $button_link; ?>"><?php print t('Add own event!'); ?></a>
+        <a class="button" href="<?php print $button_link; ?>"><?php print t($btn_lisa_text); ?></a>
     </p>
 	</div>
 </div>
