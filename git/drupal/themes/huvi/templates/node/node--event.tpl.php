1455ecdf89ae4e2b3aadef943333b2affc704fee
juuliabellcom <juuliadevadze@gmail.com>
2017-02-17 00:35:24 +0200

    formmatting

--- git/drupal/themes/huvi/templates/node/node--event.tpl.php
+++ git/drupal/themes/huvi/templates/node/node--event.tpl.php
@@ -185,12 +185,12 @@ if(isset($event) && !empty($event)):
 		 <?php //Korraldaja 
 		  if(isset($event['korraldaja']) && !empty($event['korraldaja'])): ?>
 			<?php foreach($event['korraldaja'] as $korraldaja_item): ?>
-        <p><b> <?php print t('Korraldaja') ?> </b><br/>
-          <?php print($korraldaja_item['name']); ?>, <?php print($korraldaja_item['email']); ?> <br/>
-			    <?php print t('kontekttelefon') ?>: <?php print($korraldaja_item['phone']); ?> </p>
-			</div>
-		  <?php endforeach; ?>
-		    <?php endif; ?>
+			   <p><b> <?php print t('Korraldaja') ?> </b><br/>
+			   <?php print($korraldaja_item['name']); ?>, <?php print($korraldaja_item['email']); ?> <br/>
+			   <?php print t('kontekttelefon') ?>: <?php print($korraldaja_item['phone']); ?> </p>			
+		        <?php endforeach; ?>
+		  <?php endif; ?>
+		 </div>
 
         <?php if(isset($node->field_videos)): ?>
           <?php print render($content['field_videos']); ?>
