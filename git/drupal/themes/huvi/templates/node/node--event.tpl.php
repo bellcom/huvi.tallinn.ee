6e2d03533c26c1ddcca78bd87bfd4e573ed5a2b8
riivobellcom <riivo.erik@bellcom.ee>
2017-10-05 13:10:56 +0300

    Placed 'korraldaja' phone and email together on separate row.

--- git/drupal/themes/huvi/templates/node/node--event.tpl.php
+++ git/drupal/themes/huvi/templates/node/node--event.tpl.php
@@ -190,7 +190,8 @@ if(isset($event) && !empty($event)):
 		  if(isset($event['korraldaja']) && !empty($event['korraldaja'])): ?>
 			<?php foreach($event['korraldaja'] as $korraldaja_item): ?>
 			   <p><b> <?php print t('Korraldaja') ?> </b><br/>
-			   <?php print($korraldaja_item['name']); ?>, <?php print($korraldaja_item['email']); ?> <br/>
+			   <?php print($korraldaja_item['name']); ?><br/>
+               <?php print($korraldaja_item['email']); ?>,
 			   <?php print t('kontakttelefon') ?>: <?php print($korraldaja_item['phone']); ?> </p>			
 		        <?php endforeach; ?>
 		  <?php endif; ?>
