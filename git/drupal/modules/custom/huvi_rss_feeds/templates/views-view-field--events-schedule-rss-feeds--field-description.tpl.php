8796b4d8e4c15e881f911386c05069c9358ea434
juuliabellcom <juulia@bellcom.dk>
2017-11-03 15:53:20 +0200

    HOTFIX rss feeds issue

---
+++ git/drupal/modules/custom/huvi_rss_feeds/templates/views-view-field--events-schedule-rss-feeds--field-description.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+print _huvi_rss_feeds_clear_html($output);
