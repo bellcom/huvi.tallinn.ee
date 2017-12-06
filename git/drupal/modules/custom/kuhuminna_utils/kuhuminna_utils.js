385226668056dc11aa3e9eebc7e0e1160c1d33de
Juulia Devadze <jd@est1>
2017-12-06 13:10:51 +0100

    HOTFIX Date field issue

--- git/drupal/modules/custom/kuhuminna_utils/kuhuminna_utils.js
+++ git/drupal/modules/custom/kuhuminna_utils/kuhuminna_utils.js
@@ -11,7 +11,8 @@
     });
 
     format_date_field(); //format field sheduler
-
+$('#edit-field-schedule-und-0-field-schedule-date-und-0-value-timeEntry-popup-1').focus();
+$('html,body').scrollTop(0);
     $('.date-start').change(function() {
       var fieldset;
       fieldset = $(this).parents('.fieldset-wrapper');
