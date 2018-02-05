e0f5e6350468ab893e26d207d6274379246b9b8c
juuliabellcom <juulia@bellcom.dk>
2018-02-06 00:20:12 +0200

    huvi events/activities open data xml feed

---
+++ git/drupal/modules/custom/huvi_rss_feeds/templates/views-view-row-rss--events-schedule-rss-feeds--feeds-schedule.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Default view template to display a item in an RSS feed.
+ *
+ * @ingroup views_templates
+ */
+$format = $_GET['format'];
+?>
+<item>
+  <title><?php print $title; ?></title>
+  <link><?php print $link; ?></link>
+  <?php if ($format == 1 || $format == 2) : ?>
+    <description><?php print $description; ?></description>
+  <?php endif; ?>
+  <?php if ($format == 2) :  ?>
+
+    <?php $row_index = 0;
+    if (isset($view->row_index)) {
+      $row_index = $view->row_index;    }
+
+    $node = node_load($view->result[$row_index]->field_schedule_field_collection_item_nid); ?>
+
+    <?php if (file_exists(drupal_realpath($node->field_gallery['und'][0]['uri']))): ?>
+      <enclosure url="<?php print file_create_url($node->field_gallery['und'][0]['uri']); ?>" type="image/*" />
+      <?php elseif ($image = $node->field_image_url['und'][0]['value']): ?>
+      <enclosure url="<?php print $image ?>" type="image/*" />
+    <?php endif; ?>
+  <?php endif; ?>
+  <?php print $item_elements; ?>
+
+</item>
