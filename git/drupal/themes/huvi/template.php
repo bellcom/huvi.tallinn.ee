e721d9c4dd9d32d64a565c5a30ff5c9bef1c7f34
juuliabellcom <juuliadevadze@gmail.com>
2018-04-04 14:17:37 +0300

    Roof event without the date must be findable via search

--- git/drupal/themes/huvi/template.php
+++ git/drupal/themes/huvi/template.php
@@ -32,3 +32,15 @@ function huvi_omega_layout_alter(&$layout) {
     $layout = 'landing';
   }
 }
+
+function huvi_preprocess_views_view(&$variables) {
+  if ($variables['view']->name  == 'event_listing_fixed') {
+     if (!empty($variables['attachment_before'])) {
+    // The line below is what is returning 0 regardless of how many rows are in the attachment.
+    $attachment = $variables['attachment_before'];
+    if (!empty($variables['empty']) && strpos( $attachment, "view-content") !== FALSE) {
+      unset($variables['empty']);
+    }
+  }
+  }
+}
--- git/drupal/themes/huvi/templates/views/views-view-field--field-schedule-date.tpl.php
+++ git/drupal/themes/huvi/templates/views/views-view-field--field-schedule-date.tpl.php
@@ -1,9 +1,11 @@
 <?php
-
-if (date("H:i", $row->field_data_field_schedule_date_field_schedule_date_value) == "23:55"){
+if (!isset($row->field_data_field_schedule_date_field_schedule_date_value)) {
+  print t('Katusüritus');
+}
+else if (date("H:i", $row->field_data_field_schedule_date_field_schedule_date_value) == "23:55"){
   print '<span class="date-display-single">';
   print t('Kogu päev');
   print '<span>';
-} 
+}
 else
   print $output;
