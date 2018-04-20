3b8946f39256d473d3de5c7e7e4b628504cd3b7c
juuliabellcom <juuliadevadze@gmail.com>
2018-04-20 10:53:06 +0300

    Added missing JS files

---
+++ git/drupal/themes/huvi/js/huvi.subevents.filters.js
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function ($) {
+
+  var filter_type;
+  var type = [];
+  var district = [];
+  var date = [];
+  var start_date = null;
+  var end_date = null;
+  var currentDate = new Date();
+  var dateRangePickerConfig = {
+    autoClose: false,
+    format: 'DD.MM.YYYY',
+    separator: ' - ',
+    language: 'et',
+    autoClose: false,
+    startOfWeek: 'monday',
+    alwaysOpen: false,
+    startDate: currentDate.getDate() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getFullYear(),
+    getValue: function ()
+    {
+      return $(this).val();
+    },
+    setValue: function (s, s1, s2) {
+      if ((s1 == "" && s2 == "") || (typeof s1 === 'undefined' && typeof s2 == 'undefined'))
+        this.innerHTML = 'Vali ajavahemik';
+      if (s2 == s1) {
+        var parts = s1.split(".");
+        date = new Date(parts[2], parts[1] - 1, parts[0]);
+        this.innerHTML = s1 + ' | ' + getWeekday(date.getDay());
+      } else
+        this.innerHTML = s;
+    },
+  }
+  function clearArray(array) {
+    while (array.length) {
+      array.pop();
+    }
+  }
+  function getWeekday(day) {
+    var weekdays = ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'];
+    return weekdays[day];
+  }
+  function getActiveFilters() {
+    clearArray(date);
+    clearArray(type);
+    clearArray(district);
+    $('ul.subevent-filter-tabs li.active').each(function (index, value) {
+      if (!$(this).hasClass('label')) {
+        filter_type = $(this).parents('ul.subevent-filter-tabs').attr('id');
+        if (filter_type == 'type') {
+          type.push($(this).children('a').attr('id'));
+        } else if (filter_type == 'district') {
+          district.push($(this).children('a').attr('id'));
+        } else if (filter_type == 'date') {
+          date.push($(this).children('a').attr('id'));
+        }
+      }
+      ;
+    });
+  }
+  function subeventsFilterMobileCloseAll() {
+    $('ul.subevent-filter-tabs').removeClass('open');
+    $('ul.subevent-filter-tabs li').removeClass('mobileOpen');
+  }
+  function subeventsFilterMobileClick(el) {
+    subeventsFilterMobileCloseAll();
+    el.find('li').each(function (index, value) {
+      if ($(this).hasClass('label') == false) {
+        if ($(this).hasClass('mobileOpen') ) {
+          $(this).removeClass('mobileOpen');
+        }
+        else {
+          
+          $(this).addClass('mobileOpen');
+        }
+      }
+    el.addClass('open');
+    });    
+  }     
+  Drupal.behaviors.subevents = {attach: function () {
+    if ($('#period').length) {
+      $('#period').dateRangePicker(dateRangePickerConfig)
+                .bind('datepicker-apply', function (event, obj) {
+                  if (obj.date2 == 'Invalid Date' || obj.date2 == obj.date1) {
+                    obj.date2 = obj.date1;
+                    obj.value = (obj.date1.getDate() < 10 ? '0' + obj.date1.getDate() : obj.date1.getDate()) + '.' + ((obj.date1.getMonth() + 1) < 10 ? '0'
+                            + (obj.date1.getMonth() + 1) : (obj.date1.getMonth() + 1)) + '.' + obj.date1.getFullYear()
+                            + ' | ' + getWeekday(obj.date1.getDay());
+                  }
+                  this.innerHTML = obj.value;
+                  start_date = obj.date1.getDate() + '.' + (obj.date1.getMonth() + 1) + '.' + obj.date1.getFullYear() + ' 00:00:00';
+                  end_date = obj.date2.getDate() + '.' + (obj.date2.getMonth() + 1) + '.' + obj.date2.getFullYear() + ' 23:59:59';
+
+                  getActiveFilters();
+                  parent_node_array = $(this).parents('.subevents-block').attr('id').split("-");
+                  $('#modal_loader').show();
+                  $.ajax({
+                    type: 'POST',
+                    url: '/subevents/get/ajax',
+                    data: {filters: {date, type, district}, nid: parent_node_array[1], start_date: start_date, end_date: end_date},
+                    dataType: "html", // Type of the content we're expecting in the response
+                    success: function (data) {
+                      // alert(data.children('.event-group'));
+                      $('#sub_events').html(data);  // Place AJAX content inside the ajax wrapper div
+                      $('#modal_loader').hide();
+                    }
+                  });
+                })
+
+    }
+      $('a.subevent-filter-tab').click(function (e) {
+        e.preventDefault();
+
+        filter_type = $(this).parents('ul.subevent-filter-tabs').attr('id');
+        if (filter_type == 'date') {
+          $(this).parents('ul.subevent-filter-tabs').children('li').each(function () {
+            $(this).removeClass('active')
+          });
+          if ($(this).attr('id') != 'period') {
+            start_date = null;
+            end_date = null;
+          }
+        }
+
+        $(this).parent('li').toggleClass('active');
+        if ($(this).attr('id') == 'period')
+          return false;
+
+
+        getActiveFilters();
+        parent_node_array = $(this).parents('.subevents-block').attr('id').split("-");
+         $('#modal_loader').show();
+        $.ajax({
+          type: 'POST',
+          url: '/subevents/get/ajax',
+          data: {filters: {date, type, district}, nid: parent_node_array[1], start_date: start_date, end_date: end_date},
+          dataType: "html", // Type of the content we're expecting in the response
+          success: function (data) {
+            // alert(data.children('.event-group'));
+            $('#sub_events').html(data);  // Place AJAX content inside the ajax wrapper div
+             $('#modal_loader').hide();
+          }
+        });
+        return false;
+      });
+      $('ul.subevent-filter-tabs li.label').click(function () {
+        subeventsFilterMobileClick($(this).parent());
+        })
+    }
+  };
+})(jQuery);
