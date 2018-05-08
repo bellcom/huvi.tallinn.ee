637998b888cdfd6b9d65bd7182c205325823e444
juuliabellcom <juuliadevadze@gmail.com>
2018-05-08 22:04:49 +0300

    Change the non-existing response 404 to LehekÃ¼lge ei leitud

--- git/drupal/themes/huvi/template.php
+++ git/drupal/themes/huvi/template.php
@@ -13,6 +13,11 @@
  *   layout
  */
 function huvi_omega_layout_alter(&$layout) {
+  $header = drupal_get_http_header('status');
+  if ($header == '404 Not Found') {
+    $layout = 'main';
+    return;
+  }
   if ((arg(0) == 'uritus' || arg(0) == 'huvitegevus')) {
     $layout = 'single';
   }
