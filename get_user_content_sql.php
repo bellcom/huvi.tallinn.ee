104eb3bb0d7119075c99072e7bf8f4c248880ed2
Dmitri Serõn <ds@bellcom.ee>
2019-04-18 10:02:15 +0300

    CLI scripts updated.

--- get_user_content_sql.php
+++ get_user_content_sql.php
@@ -14,7 +14,7 @@ print('Drupal bootstrap done..' . PHP_EOL);
 $user_id = $_SERVER['argv'][6];
 print('Loading data for user: ' . $user_id . PHP_EOL);
 $id = array();
-
+// node
 $query = db_query('SELECT n.nid
 FROM {node} n WHERE n.uid = :uid', array(':uid' => $user_id));
 $result = $query->fetchAll();
@@ -25,7 +25,7 @@ $id_list = implode(', ', $id);
 print_r("UPDATE node SET node.uid = ? WHERE node.nid IN ( $id_list);" . PHP_EOL);
 print_r(PHP_EOL);
 $id = array();
-
+// node_revision
 $query = db_query('SELECT n.nid
 FROM {node_revision} n WHERE n.uid = :uid', array(':uid' => $user_id));
 $result = $query->fetchAll();
@@ -33,21 +33,10 @@ foreach ($result as $record) {
     $id[] = $record->nid;
 }
 $id_list = implode(', ', $id);
-print_r("UPDATE node_revision SET node_revision.uid = ? WHERE node.nid IN ( $id_list);" . PHP_EOL);
+print_r("UPDATE node_revision SET node_revision.uid = ? WHERE node_revision.nid IN ( $id_list);" . PHP_EOL);
 print_r(PHP_EOL);
 $id = array();
-
-$query = db_query('SELECT n.aid
-FROM {authmap} n WHERE n.uid = :uid', array(':uid' => $user_id));
-$result = $query->fetchAll();
-foreach ($result as $record) {
-    $id[] = $record->aid;
-}
-$id_list = implode(', ', $id);
-print_r("UPDATE authmap SET authmap.uid = ? WHERE authmap.aid IN ( $id_list);" . PHP_EOL);
-print_r(PHP_EOL);
-$id = array();
-
+// file_managed
 $query = db_query('SELECT n.fid
 FROM {file_managed} n WHERE n.uid = :uid', array(':uid' => $user_id));
 $result = $query->fetchAll();
@@ -55,7 +44,7 @@ foreach ($result as $record) {
     $id[] = $record->fid;
 }
 $id_list = implode(', ', $id);
-print_r("UPDATE file_managed SET file_managed.uid = ? WHERE file_managed.aid IN ( $id_list);" . PHP_EOL);
+print_r("UPDATE file_managed SET file_managed.uid = ? WHERE file_managed.fid IN ( $id_list);" . PHP_EOL);
 print_r(PHP_EOL);
 
 print('Finish.' . PHP_EOL);
